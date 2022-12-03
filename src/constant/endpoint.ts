96d8fc60b115b3c8263aa223631f3a4126789883
Ade Yahya <adeyahyaprasetyo@gmail.com>
2022-12-03 20:29:48 +0700

    improve contract factory

---
+++ src/constant/endpoint.ts
@@ -0,0 +1,10 @@
+export const WS_ENDPOINTS = {
+  "0x38": "wss://bsc-ws-node.nariox.org:443",
+  "0x61":
+    "wss://bsc.getblock.io/testnet/?api_key=f2e715e6-8838-4230-83d2-62c6f1ad9a44",
+};
+
+export const RPC_ENDPOINTS = {
+  "0x38": "https://bsc-dataseed1.ninicoin.io",
+  "0x61": "https://data-seed-prebsc-1-s3.binance.org:8545/",
+};
--- src/lib/contractFactory.ts
+++ src/lib/contractFactory.ts
@@ -1,6 +1,59 @@
+declare module globalThis {
+  var providerCache: Record<string, any>;
+}
+
 import detectEthereumProvider from "@metamask/detect-provider";
+import { RPC_ENDPOINTS } from "constant/endpoint";
 import { ethers } from "ethers";
+import { abi as valhallaAbi } from "@warmbyte/valhalla/artifacts/contracts/Valhalla.sol/Valhalla.json";
+import { Valhalla } from "@warmbyte/valhalla/typechain-types";
+
+const CURRENT_CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID || "0x61";
+const ENDPOINT = RPC_ENDPOINTS[CURRENT_CHAIN_ID as "0x61"];
+
+/**
+ * function to get provider asyncronously and memoize it
+ * @param key cacheKey
+ * @param fn async function that to initiate provider if not in cache
+ * @returns provider
+ */
+const getFromCache = async <T>(
+  key: string,
+  fn: () => Promise<any>
+): Promise<T> => {
+  if (!globalThis.providerCache) globalThis.providerCache = {};
+  if (!globalThis.providerCache[key]) {
+    globalThis.providerCache[key] = await fn();
+  }
+
+  return globalThis.providerCache[key];
+};
 
 export const getWallet = async () => {
-  const wallet = await detectEthereumProvider<ethers.providers.Web3Provider>();
+  const wallet = await getFromCache<ethers.providers.Web3Provider>(
+    "wallet",
+    detectEthereumProvider
+  );
+
+  // whenever the chain from metamask changed
+  // just hard reload the web to reset all state
+  wallet.on("chainChanged", () => {
+    window.location.reload();
+  });
+};
+
+export const getMainProvider = async () => {
+  return await getFromCache<ethers.providers.JsonRpcProvider>(
+    "mainProvider",
+    async () => new ethers.providers.JsonRpcProvider(ENDPOINT)
+  );
+};
+
+export const getValhallaContract = async () => {
+  const provider = await getMainProvider();
+  const contract = await getFromCache<Valhalla>(
+    "valhallaContract",
+    async () => new ethers.Contract("", valhallaAbi, provider)
+  );
+  return contract;
 };

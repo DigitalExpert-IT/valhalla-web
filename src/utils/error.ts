abd9763a67cc80c6419db87839cf90c2abdc2876
mananjabrik <manan.jabrik@gmail.com>
2022-12-20 01:31:50 +0700

    add error type

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,3 +1,14 @@
+interface IErrorType {
+  argument: string;
+  code: string;
+  reason: string;
+  value: string;
+  data: {
+    message: string;
+  };
+  message: string;
+}
+
 const format = (message?: string | null) => {
   if (!message) return message;
   return message
@@ -6,7 +17,8 @@ const format = (message?: string | null) => {
     .replace(/\s\(.+/, "");
 };
 
-export const getErrorMessage = (error: any) => {
+export const getErrorMessage = (error: IErrorType) => {
+  if (error?.reason) return format(error.reason);
   if (error?.data?.message) return format(error.data.message);
   if (error?.message) return format(error.message);
   return "Unkown Error";

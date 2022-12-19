d89b8151b2a83dd90aa7add652ce58d6525b166b
Ade Yahya Prasetyo <adeyahyaprasetyo@gmail.com>
2022-12-20 03:43:46 +0700

    feat: apply registration design (#21)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,29 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, useColorMode } from "@chakra-ui/react";
 import theme from "theme";
 import NiceModal from "@ebay/nice-modal-react";
 import "locales";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <NiceModal.Provider>
-        <Component {...pageProps} />
+        <Main {...props} />
       </NiceModal.Provider>
     </ChakraProvider>
   );
 }
+
+const Main = ({ Component, pageProps }: AppProps) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  // enforce dark mode
+  useEffect(() => {
+    if (colorMode !== "dark") {
+      toggleColorMode();
+    }
+  }, [colorMode]);
+
+  return <Component {...pageProps} />;
+};

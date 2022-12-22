58614efa9a18b3653d1344cd0c000bb044565227
Ade Yahya Prasetyo <adeyahyaprasetyo@gmail.com>
2022-12-22 21:05:52 +0700

    feat: adjust font & colors (#35)

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,20 +1,37 @@
-import { Container, Heading, Text, Card, CardBody } from "@chakra-ui/react";
+import {
+  Container,
+  Heading,
+  Text,
+  Card,
+  CardBody,
+  Box,
+} from "@chakra-ui/react";
 import { FormRegister } from "components";
 import { Trans } from "react-i18next";
 
 const Register = () => {
   return (
     <Container maxW="4xl">
-      <Heading mt={{ base: "12", sm: "24" }} as="h1" textAlign="center">
-        <Trans
-          i18nKey="pages.register.title"
-          components={{
-            strong: (
-              <Text as="span" variant="gradient" colorScheme="orange:pink" />
-            ),
-          }}
-        />
-      </Heading>
+      <Box as="header" textAlign="center" px={{ base: "8", lg: "4" }}>
+        <Heading mt={{ base: "24", sm: "36" }} as="h1" textAlign="center">
+          <Trans
+            i18nKey="pages.register.title"
+            components={{
+              strong: (
+                <Text as="span" variant="gradient" colorScheme="orange:pink" />
+              ),
+            }}
+          />
+        </Heading>
+        <Text mt="4" fontSize="lg">
+          <Trans
+            i18nKey="pages.register.subtitle"
+            components={{
+              strong: <Text as="span" color="secondary.500" />,
+            }}
+          />
+        </Text>
+      </Box>
       <Card
         variant="gradient"
         colorScheme="purple:pink"
--- src/utils/address.ts
+++ src/utils/address.ts
@@ -1,4 +1,5 @@
 export const shortenAddress = (address: string) => {
+  if (!address) return "0x0";
   if (address.length < 42) return address;
   const upper = address.toUpperCase();
   const head = upper.slice(0, 5);

86df28343bcc55bdfe84bd0489e0a6923e6ea2ff
Y0SH4 <yosa.agung13@gmail.com>
2022-12-26 19:02:23 +0700

    remove hello

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
 import { LayoutMain } from "components";
-import { Box } from "@chakra-ui/react";
 
 export default function Home() {
-  return (
-    <LayoutMain>
-      <Box mb="100vh">Hello</Box>
-    </LayoutMain>
-  );
+  return <LayoutMain>{null}</LayoutMain>;
 }

e38ee94c51c8a3159351b61573a5282ff166d76c
mananjabrik <manan.jabrik@gmail.com>
2022-12-27 20:20:59 +0700

    create globe animation

---
+++ src/components/animation/globe.tsx
@@ -0,0 +1,35 @@
+import { Canvas, MeshProps, useFrame, useLoader } from "@react-three/fiber";
+import { Suspense, useRef } from "react";
+import { Mesh, TextureLoader } from "three";
+// import { OrbitControls } from "@react-three/drei";
+
+interface GlobeMesProps extends MeshProps {}
+
+const GlobeMesh: React.FC<GlobeMesProps> = props => {
+  const ref = useRef<Mesh>(null);
+  const colorMap = useLoader(TextureLoader, "globe/globe.jpg");
+  useFrame((_, delta) => {
+    if (!ref.current) return;
+    return (ref.current.rotation.y += 0.01);
+  });
+
+  return (
+    <mesh {...props} ref={ref} scale={2}>
+      <sphereGeometry />
+      <meshStandardMaterial map={colorMap} roughness={0.5} metalness={0.5} />
+    </mesh>
+  );
+};
+
+export const Globe = () => {
+  return (
+    <Canvas>
+      <Suspense fallback={null}>
+        <ambientLight intensity={0.8} />
+        <pointLight position={[10, 10, 10]} />
+        <GlobeMesh />
+        {/* <OrbitControls /> */}
+      </Suspense>
+    </Canvas>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import { Box } from "@chakra-ui/react";
 import { LayoutMain } from "components";
+import { Globe } from "components/animation/globe";
 
 export default function Home() {
-  return <LayoutMain>{null}</LayoutMain>;
+  return (
+    <LayoutMain>
+      {
+        <Box h={"100vh"}>
+          <Globe></Globe>
+        </Box>
+      }
+    </LayoutMain>
+  );
 }

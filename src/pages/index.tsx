a53f2323b1585643e27b3c9e0d8591efa6f4acc4
mananjabrik <manan.jabrik@gmail.com>
2022-12-27 20:46:06 +0700

    remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-import { Box } from "@chakra-ui/react";
 import { LayoutMain } from "components";
-import { Globe } from "components/animation/globe";
 
 export default function Home() {
   return <LayoutMain>{null}</LayoutMain>;

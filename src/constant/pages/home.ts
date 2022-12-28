77fa0a335f11f4da5daef68a9d712523edc8ae3b
Ade Yahya <adeyahyaprasetyo@gmail.com>
2022-12-28 10:07:21 +0700

    refactor: header

---
+++ src/constant/pages/home.ts
@@ -0,0 +1,8 @@
+import { t } from "i18next";
+
+export const HEADER_IMAGE_DATA = [
+  { uri: "/images/imgHeader1.png", alt: t("common.nftFarming") },
+  { uri: "/images/imgHeader2.png", alt: t("common.shareToEarn") },
+  { uri: "/images/imgHeader3.png", alt: t("common.royaltyProperty") },
+  { uri: "/images/imgHeader4.png", alt: t("common.metaverseProject") },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,42 @@
-import { Box } from "@chakra-ui/react";
-import { LayoutMain } from "components";
-import { HeaderHome } from "components/Header";
+import { Heading, Text, Box, HStack, Button } from "@chakra-ui/react";
+import { Trans, useTranslation } from "react-i18next";
+import Link from "next/link";
+import { LayoutMain, WidgetMainHeader } from "components";
+import { HEADER_IMAGE_DATA } from "constant/pages/home";
 
 export default function Home() {
+  const { t } = useTranslation();
+
   return (
     <LayoutMain>
-      <HeaderHome />
+      <WidgetMainHeader
+        pt={{ base: "0", lg: "16" }}
+        imageData={HEADER_IMAGE_DATA}
+      >
+        <Box px={{ base: "4", lg: "16" }} py={{ base: "0", lg: "16" }}>
+          <Heading textAlign={{ base: "center", lg: "initial" }} as="h1">
+            <Trans
+              i18nKey="pages.home.header.title"
+              components={{
+                strong: <Text as="span" color="secondary.500" />,
+              }}
+            />
+          </Heading>
+          <Text
+            textAlign={{ base: "center", lg: "initial" }}
+            mt="4"
+            fontSize="lg"
+          >
+            <Trans i18nKey="pages.home.header.subtitle" />
+          </Text>
+          <HStack justify={{ base: "center", lg: "start" }} mt="6" spacing="4">
+            <Link href="/register">
+              <Button colorScheme="brand">{t("common.register")}</Button>
+            </Link>
+            <Button colorScheme="brand">Telegram</Button>
+          </HStack>
+        </Box>
+      </WidgetMainHeader>
     </LayoutMain>
   );
 }

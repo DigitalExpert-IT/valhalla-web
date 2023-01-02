c98756b2e5236ad2d89698821792e0a517412abc
mananjabrik <manan.jabrik@gmail.com>
2023-01-02 07:26:05 +0700

    setup example swap

--- src/components/Form/FormSwap.tsx
+++ src/components/Form/FormSwap.tsx
@@ -1,32 +1,67 @@
 import { Badge, Button, FormControl, Select, Stack } from "@chakra-ui/react";
-import { FormInput } from "components/FormUtils";
+import { FormInput, FormSelect } from "components/FormUtils";
+import { fromBn } from "evm-bn";
 import { useSwap } from "hooks";
+import { useEffect, useMemo, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { validateRequired } from "utils";
+
+interface ISwapToken {
+  quantity: number;
+  tokenSelect: string;
+}
 
 export const FormSwap = () => {
-  const swap = useSwap();
-  console.log(swap.currency.gnet.pair);
-  const { register, handleSubmit, control } = useForm();
-  const onSwap = (e: any) => {
-    console.log("quantity", e.quantity);
-  };
+  const { currency, swapToken } = useSwap();
+  const { handleSubmit, control } = useForm<ISwapToken>();
+  const { t } = useTranslation();
+  const [price, setPrice] = useState(0);
+  const [selectCurrency, setSelecCurrency] = useState<string | undefined>();
+
+  const onSubmit = handleSubmit(data => {
+    console.log(data);
+  });
+
+  const normalizeCurrencies = useMemo(() => {
+    return Object.values(currency).map(c => {
+      const USDTPair = c.pair.name === "USDT";
+      return {
+        ...c,
+        value: c.pair.name,
+        child: USDTPair ? "GNET / USDT" : "USDT / GNET",
+      };
+    });
+  }, [currency]);
+
+  useEffect(() => {}, []);
+
   return (
-    <Stack>
+    <Stack as="form" onSubmit={onSubmit}>
       <Stack direction={"row"} alignItems="center">
-        <FormControl>
-          <Select placeholder="Token">
-            <option value="USDT">USDT</option>
-            <option value="GNET">GNET</option>
-          </Select>
-        </FormControl>
+        <FormSelect
+          control={control}
+          name="tokenSelect"
+          placeholder={t("form.placeholder.selectCurrency")}
+          onChange={e => setSelecCurrency(e.target.value)}
+          option={normalizeCurrencies}
+        ></FormSelect>
         <FormInput
           control={control}
-          name="Quantity"
-          placeholder={"Quantity"}
+          name="quantity"
+          rules={{
+            required: validateRequired(t("form.label.swap")),
+            minLength: 1,
+          }}
+          placeholder={t("form.placeholder.quantity")}
         ></FormInput>
-        <Badge>100 USDT</Badge>
+        {selectCurrency && (
+          <Badge>
+            {price} {selectCurrency}
+          </Badge>
+        )}
       </Stack>
-      <Button type="submit">Swap</Button>
+      <Button type="submit">{t("common.swap")}</Button>
     </Stack>
   );
 };
---
+++ src/components/FormUtils/FormSelect.tsx
@@ -0,0 +1,63 @@
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Select,
+} from "@chakra-ui/react";
+import { ChangeEventHandler } from "react";
+import {
+  Controller,
+  ControllerProps,
+  FieldValues,
+  FieldError,
+  FieldPath,
+  get,
+  useFormState,
+} from "react-hook-form";
+
+type Props<T extends FieldValues, TName extends FieldPath<T>> = Omit<
+  ControllerProps<T, TName>,
+  "render"
+> & {
+  label?: string | null;
+  placeholder?: string | null;
+  helperText?: string | null;
+  option: { value: string; child: string }[];
+  onChange?: ChangeEventHandler<HTMLSelectElement> | undefined;
+};
+
+export const FormSelect = <T extends FieldValues, TName extends FieldPath<T>>(
+  props: Props<T, TName>
+) => {
+  const { errors } = useFormState({ control: props.control });
+  const error: FieldError | null = get(errors, props.name);
+
+  return (
+    <FormControl isRequired={!!props.rules?.required} isInvalid={!!error}>
+      {props.label ? <FormLabel>{props.label}</FormLabel> : null}
+      <Controller
+        control={props.control}
+        defaultValue={props.defaultValue}
+        rules={props.rules}
+        name={props.name}
+        render={({ field: { value, ...rest } }) => (
+          <Select
+            {...rest}
+            defaultValue={value ?? ""}
+            placeholder={props.placeholder ?? ""}
+            onChange={props.onChange}
+          >
+            {props.option.map(e => (
+              <option value={e.value}>{e.child}</option>
+            ))}
+          </Select>
+        )}
+      />
+      {props.helperText ? (
+        <FormHelperText>{props.helperText}</FormHelperText>
+      ) : null}
+      <FormErrorMessage>{error?.message ?? "Invalid Value"}</FormErrorMessage>
+    </FormControl>
+  );
+};

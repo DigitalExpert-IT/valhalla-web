b88b2dacb6a4bc9089576824096ba380944119c6
mananjabrik <manan.jabrik@gmail.com>
2023-01-02 19:53:56 +0700

    setup template swap

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -1,9 +1,22 @@
+import { Card, CardBody } from "@chakra-ui/react";
 import { FormSwap, LayoutMain } from "components";
 
 const Swap = () => {
   return (
     <LayoutMain>
-      <FormSwap />
+      <Card
+        variant="gradient"
+        colorScheme="purple:pink"
+        mx="auto"
+        mt="12"
+        py="8"
+        px={{ base: "4", sm: "8" }}
+        maxW="xl"
+      >
+        <CardBody>
+          <FormSwap />
+        </CardBody>
+      </Card>
     </LayoutMain>
   );
 };

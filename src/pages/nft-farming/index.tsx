01841cf577ee361d60d9ee6ca5b51e2629f8db5b
Y0SH4 <yosa.agung13@gmail.com>
2023-01-11 05:03:46 +0700

    Initial commit

---
+++ src/pages/nft-farming/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+import { LayoutMain } from "components";
+
+const NftFarmingPage = () => {
+  return (
+    <LayoutMain>
+      <Heading>NFT Farming</Heading>
+    </LayoutMain>
+  );
+};
+
+export default NftFarmingPage;

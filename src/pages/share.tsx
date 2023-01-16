bfae9ab8e490b96579faf0d5e1b77b9581b2c544
deddybrama11 <dedybramayadi@gmail.com>
2023-01-17 01:56:46 +0700

    changes on card variant and finishing component share

--- src/pages/share.tsx
+++ src/pages/share.tsx
@@ -1,199 +1,94 @@
-import { Box, Flex, Grid, Heading, Image, Input, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Grid,
+  Heading,
+  Image,
+  Input,
+  Text,
+  Card,
+  CardBody,
+  CardHeader,
+} from "@chakra-ui/react";
 import { LayoutMain } from "components";
-import { Trans, useTranslation } from "react-i18next";
+import { SHARE_MEDIA } from "constant/pages/share";
+import { useTranslation } from "react-i18next";
 
 const Share = () => {
   const { t } = useTranslation();
-
   return (
     <LayoutMain>
       <Heading textAlign={"center"}>
         {t("pages.shareToEarn.title").toUpperCase()}
       </Heading>
 
-      <Grid templateColumns="repeat(2, 1fr)" gap={"5"} mt={"4rem"}>
-        <Box backgroundColor={"#6750A4"} p={"2rem"} borderRadius={"1rem"}>
-          <Flex gap={3}>
-            <Image
-              src={"/assets/icon/instagram.png"}
-              alt={"ig"}
-              width={70}
-              height={70}
-              objectFit={"contain"}
-            />
-            <Box>
-              <Heading>Instagram</Heading>
-              <Text fontSize={"sm"}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor
+      <Grid templateColumns={{base:"repeat(1, 1fr)",md:"repeat(2, 1fr)"}} gap={"5"} mt={"4rem"}>
+        {SHARE_MEDIA.map((item, idx) => (
+          <Card
+            key={idx}
+            variant="gradient"
+            colorScheme={"pink:purple:purple"}
+            backgroundColor={"#6750A466"}
+            borderRadius={"2xl"}
+            p={10}
+          >
+            <CardHeader p={"unset"}>
+              <Flex gap={3}>
+                <Image
+                  src={item.image}
+                  alt={"ig"}
+                  width={70}
+                  height={70}
+                  objectFit={"contain"}
+                />
+                <Flex direction={"column"} justify={"space-evenly"}>
+                  <Heading size={"lg"}>
+                    {item.mediaName.charAt(0).toUpperCase() +
+                      item.mediaName.slice(1)}
+                  </Heading>
+                  <Text fontSize={"xs"}>
+                    {t(`pages.share.item.${item.mediaName}.titleDescription`)}
+                  </Text>
+                </Flex>
+              </Flex>
+            </CardHeader>
+            <CardBody p={"unset"}>
+              <Text fontWeight={"700"} mt={5} mb={2}>
+                {t(`pages.share.item.${item.mediaName}.subtitle`)}
               </Text>
-            </Box>
-          </Flex>
-          <Box mt={"5"}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris
-          </Box>
-          <Flex mt={3} gap={2}>
-            <Box backgroundColor={"#180240"} p={2} borderRadius={"2xl"} w={"full"} opacity={"70%"}>
-              <Input
-                padding={""}
-                type={"text"}
-                fontSize={"sm"}
-                backgroundColor={"transparent"}
-                placeholder={"Paste your twitter post link here"}
-              />
-            </Box>
-            <Flex backgroundColor={"#6B5BE7"} alignItems={"center"} borderRadius={"2xl"} px={5}>
-                <Image src={"/assets/icon/send.png"} w={5} h={5} />
-            </Flex>
-          </Flex>
-        </Box>
-        <Box backgroundColor={"#6750A4"} p={"2rem"} borderRadius={"1rem"}>
-          <Flex gap={3}>
-            <Image
-              src={"/assets/icon/instagram.png"}
-              alt={"ig"}
-              width={70}
-              height={70}
-              objectFit={"contain"}
-            />
-            <Box>
-              <Heading>Instagram</Heading>
-              <Text fontSize={"sm"}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor
+              <Text>
+                {t(`pages.share.item.${item.mediaName}.subtitleDescription`)}
               </Text>
-            </Box>
-          </Flex>
-          <Box mt={"5"}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris
-          </Box>
-          <Flex mt={3} gap={2}>
-            <Box backgroundColor={"#180240"} p={2} borderRadius={"2xl"} w={"full"} opacity={"70%"}>
-              <Input
-                padding={""}
-                type={"text"}
-                fontSize={"sm"}
-                backgroundColor={"transparent"}
-                placeholder={"Paste your twitter post link here"}
-              />
-            </Box>
-            <Flex backgroundColor={"#6B5BE7"} alignItems={"center"} borderRadius={"2xl"} px={5}>
-                <Image src={"/assets/icon/send.png"} w={5} h={5} />
-            </Flex>
-          </Flex>
-        </Box>        <Box backgroundColor={"#6750A4"} p={"2rem"} borderRadius={"1rem"}>
-          <Flex gap={3}>
-            <Image
-              src={"/assets/icon/instagram.png"}
-              alt={"ig"}
-              width={70}
-              height={70}
-              objectFit={"contain"}
-            />
-            <Box>
-              <Heading>Instagram</Heading>
-              <Text fontSize={"sm"}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor
-              </Text>
-            </Box>
-          </Flex>
-          <Box mt={"5"}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris
-          </Box>
-          <Flex mt={3} gap={2}>
-            <Box backgroundColor={"#180240"} p={2} borderRadius={"2xl"} w={"full"} opacity={"70%"}>
-              <Input
-                padding={""}
-                type={"text"}
-                fontSize={"sm"}
-                backgroundColor={"transparent"}
-                placeholder={"Paste your twitter post link here"}
-              />
-            </Box>
-            <Flex backgroundColor={"#6B5BE7"} alignItems={"center"} borderRadius={"2xl"} px={5}>
-                <Image src={"/assets/icon/send.png"} w={5} h={5} />
-            </Flex>
-          </Flex>
-        </Box>        <Box backgroundColor={"#6750A4"} p={"2rem"} borderRadius={"1rem"}>
-          <Flex gap={3}>
-            <Image
-              src={"/assets/icon/instagram.png"}
-              alt={"ig"}
-              width={70}
-              height={70}
-              objectFit={"contain"}
-            />
-            <Box>
-              <Heading>Instagram</Heading>
-              <Text fontSize={"sm"}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor
-              </Text>
-            </Box>
-          </Flex>
-          <Box mt={"5"}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris
-          </Box>
-          <Flex mt={3} gap={2}>
-            <Box backgroundColor={"#180240"} p={2} borderRadius={"2xl"} w={"full"} opacity={"70%"}>
-              <Input
-                padding={""}
-                type={"text"}
-                fontSize={"sm"}
-                backgroundColor={"transparent"}
-                placeholder={"Paste your twitter post link here"}
-              />
-            </Box>
-            <Flex backgroundColor={"#6B5BE7"} alignItems={"center"} borderRadius={"2xl"} px={5}>
-                <Image src={"/assets/icon/send.png"} w={5} h={5} />
-            </Flex>
-          </Flex>
-        </Box>        <Box backgroundColor={"#6750A4"} p={"2rem"} borderRadius={"1rem"}>
-          <Flex gap={3}>
-            <Image
-              src={"/assets/icon/instagram.png"}
-              alt={"ig"}
-              width={70}
-              height={70}
-              objectFit={"contain"}
-            />
-            <Box>
-              <Heading>Instagram</Heading>
-              <Text fontSize={"sm"}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor
-              </Text>
-            </Box>
-          </Flex>
-          <Box mt={"5"}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris
-          </Box>
-          <Flex mt={3} gap={2}>
-            <Box backgroundColor={"#180240"} p={2} borderRadius={"2xl"} w={"full"} opacity={"70%"}>
-              <Input
-                padding={""}
-                type={"text"}
-                fontSize={"sm"}
-                backgroundColor={"transparent"}
-                placeholder={"Paste your twitter post link here"}
-              />
-            </Box>
-            <Flex backgroundColor={"#6B5BE7"} alignItems={"center"} borderRadius={"2xl"} px={5}>
-                <Image src={"/assets/icon/send.png"} w={5} h={5} />
-            </Flex>
-          </Flex>
-        </Box>
+              <Flex mt={3} gap={2}>
+                <Box
+                  backgroundColor={"#180240"}
+                  p={2}
+                  borderRadius={"2xl"}
+                  w={"full"}
+                  opacity={"70%"}
+                >
+                  <Input
+                    padding={""}
+                    type={"text"}
+                    fontSize={"sm"}
+                    backgroundColor={"transparent"}
+                    placeholder={
+                      t(`pages.share.placeholder`, { media: item.mediaName })!
+                    }
+                  />
+                </Box>
+                <Flex
+                  backgroundColor={"#6B5BE7"}
+                  alignItems={"center"}
+                  borderRadius={"2xl"}
+                  px={5}
+                >
+                  <Image src={"/assets/icon/send.png"} w={5} h={5} />
+                </Flex>
+              </Flex>
+            </CardBody>
+          </Card>
+        ))}
       </Grid>
     </LayoutMain>
   );

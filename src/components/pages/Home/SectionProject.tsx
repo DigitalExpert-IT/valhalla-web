c01650f0a38eff12d846c855853701a2e2ebc5a6
mananjabrik <manan.jabrik@gmail.com>
2023-03-16 22:42:28 +0700

    update

--- src/components/pages/Home/SectionProject.tsx
+++ src/components/pages/Home/SectionProject.tsx
@@ -18,24 +18,46 @@ export const SectionProject = () => {
   ];
   return (
     <Stack>
-      <Heading fontSize="5xl" textAlign="center">
+      <Heading
+        fontSize="5xl"
+        textAlign="center"
+        _after={{
+          content: '"PROJECT"',
+          alignSelf: "center",
+          display: "block",
+          fontSize: "180",
+          mt: "-113",
+          color: "whiteAlpha.100",
+          textAlign: "center",
+        }}
+      >
         PROJECT
       </Heading>
-      <Stack direction="row">
+      <Stack direction="row" alignItems="center">
         <Box flex={1} display="flex" justifyContent="center">
           <Image src="/assets/project/project.png" alt="project"></Image>
+          <Image
+            src="/assets/project/Group.png"
+            alt="logo"
+            pos="absolute"
+            alignSelf="center"
+          ></Image>
         </Box>
-        <Box flex={1}>
+        <Stack flex={1}>
           <Text>
             All applications and utilities will be built on a decentralized
             network platform
           </Text>
-          <UnorderedList>
+          <UnorderedList
+            listStyleType="none"
+            marginInlineStart={"none"}
+            fontSize="xs"
+          >
             {PROJECT_LIST.map((e, i) => (
               <ListItem key={i}>{e}</ListItem>
             ))}
           </UnorderedList>
-        </Box>
+        </Stack>
       </Stack>
     </Stack>
   );

3b68c112bd84d842cc2a232cd78bc034c9269517
Y0SH4 <yosa.agung13@gmail.com>
2023-03-16 14:24:06 +0700

    change logo and favicon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -61,6 +61,22 @@ export const Navbar = () => {
               isOpen={isOpen}
               onClose={onClose}
             />
+            <Link href="/">
+              <AspectRatio
+                w={isLargethan800 ? 250 : 8}
+                ratio={isLargethan800 ? 16 / 3 : 1}
+                my={{ base: "none", md: 2 }}
+              >
+                <Image
+                  src={
+                    isLargethan800
+                      ? "/assets/logo/gnLogo.png"
+                      : "/assets/logo/gn.png"
+                  }
+                  alt="logo-image"
+                />
+              </AspectRatio>
+            </Link>
             <IconButton
               variant="ghost"
               fontSize="xl"
@@ -69,22 +85,6 @@ export const Navbar = () => {
               display={{ md: "flex", lg: "none" }}
               onClick={isOpen ? onClose : onOpen}
             />
-            <Link href="/">
-              <AspectRatio
-                w={isLargethan800 ? 250 : 50}
-                ratio={isLargethan800 ? 16 / 3 : 1}
-                my="2"
-              >
-                <Image
-                  src={
-                    isLargethan800
-                      ? "/assets/logo/gnLogo.png"
-                      : "/assets/logo/logo.png"
-                  }
-                  alt="logo-image"
-                />
-              </AspectRatio>
-            </Link>
           </Stack>
           <Stack
             direction="row"

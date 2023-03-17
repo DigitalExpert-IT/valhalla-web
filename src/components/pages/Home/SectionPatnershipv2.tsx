70b28dbc87e719605183c14577dcaa3cc31e5fc3
mananjabrik <manan.jabrik@gmail.com>
2023-03-17 12:50:49 +0700

    Refactor: Patnership Component

---
+++ src/components/pages/Home/SectionPatnershipv2.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+
+export const SectionPatnershipv2 = () => {
+  return (
+    <Box>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, adipisci
+      odio. Quam repellendus, autem eveniet commodi repellat voluptatem incidunt
+      rerum! Aliquid consequatur quasi ratione ipsum, minima cum eveniet
+      possimus architecto!
+    </Box>
+  );
+};

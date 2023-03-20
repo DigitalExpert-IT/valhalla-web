172b0ef0d2befde072059194cfac7076043f6240
deddybrama11 <dedybramayadi@gmail.com>
2023-03-20 21:13:24 +0700

    Fix layout

--- src/components/Layout/LayoutMain.tsx
+++ src/components/Layout/LayoutMain.tsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { cloneElement, ReactElement } from "react";
 import { Container, Box } from "@chakra-ui/react";
 import { Navbar } from "components";
 import { LayoutFooter } from "components";
 
 interface MainProps {
-  children: React.ReactNode;
+  children: ReactElement[] | ReactElement;
 }
 
 export const LayoutMain: React.FC<MainProps> = ({ children }) => {
   return (
     <Box>
       <Navbar />
-      <Container
-        minH="55vh"
-        maxW="container.xl"
-        mb="32"
-        overflowX="hidden"
-        pt={{ base: "20", md: "40" }}
-      >
-        {children}
-      </Container>
+      {Array.isArray(children) ? (
+        children.map((cld, idx) => {
+          const child = cloneElement(cld, {
+            ...cld.props,
+            index: idx,
+            key: idx,
+          });
+          if (cld.props.withoutContainer) return child;
+          return (
+            <Container
+              key={idx}
+              minH="55vh"
+              maxW="container.xl"
+              mb="32"
+              overflowX="hidden"
+              pt={{ base: "20", md: "40" }}
+            >
+              {child}
+            </Container>
+          );
+        })
+      ) : children.props.withoutContainer ? (
+        cloneElement(children, { ...children.props, index: 0 })
+      ) : (
+        <Container
+          minH="55vh"
+          maxW="container.xl"
+          mb="32"
+          overflowX="hidden"
+          pt={{ base: "20", md: "40" }}
+        >
+          {cloneElement(children, { ...children.props, index: 0 })}
+        </Container>
+      )}
+
       <LayoutFooter />
     </Box>
   );

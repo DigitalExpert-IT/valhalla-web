c994aead870709aa7834ee35e189d5e26c4ecdfd
deddybrama11 <dedybramayadi@gmail.com>
2023-03-20 21:14:23 +0700

    Update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { OURTEAM, PARTNERSHIP } from "constant/pages/home";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Container, Heading } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import {
   SectionHeader,
@@ -13,9 +13,9 @@ import {
   TableRankBonus,
   TableRankNetwork,
   TableTokenomic,
-  BackgroundHome,
   SectionProject,
 } from "components";
+import { LayoutItem } from "components/Layout/LayoutItem";
 
 export default function Home() {
   const { t } = useTranslation();
@@ -35,21 +35,27 @@ export default function Home() {
         <WidgetTimeLine />
       </Box>
       <TableTokenomic />
-      <Box textAlign="center" my="20">
-        <Heading textTransform="uppercase">
-          {t("pages.home.teamSection")}
-        </Heading>
-        <SectionTeam data={OURTEAM} />
-      </Box>
-      <Box textAlign="center" py="20">
-        <Heading textTransform="uppercase">
-          {t("pages.home.partnershipSection")}
-        </Heading>
-        <SectionPartnership data={PARTNERSHIP} />
-      </Box>
       <Box>
         <SectionProject />
       </Box>
+      <LayoutItem
+        withoutContainer
+        bgGradient="linear(#2C1FA7 10%, #6D02C9 100%)"
+      >
+        <SectionTeam data={OURTEAM} />
+        <Container
+          minH="55vh"
+          maxW="container.xl"
+          overflowX="hidden"
+        >
+          <Box textAlign="center" py="20">
+            <Heading textTransform="uppercase">
+              {t("pages.home.partnershipSection")}
+            </Heading>
+            <SectionPartnership data={PARTNERSHIP} />
+          </Box>
+        </Container>
+      </LayoutItem>
     </LayoutMain>
   );
 }

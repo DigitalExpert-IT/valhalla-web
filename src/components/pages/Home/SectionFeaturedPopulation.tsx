41e56f775bdfae4fb17d8e3e4c2751f45cdea2fd
mananjabrik <manan.jabrik@gmail.com>
2023-03-21 10:23:03 +0700

    setup global network

--- src/components/pages/Home/SectionFeaturedPopulation.tsx
+++ src/components/pages/Home/SectionFeaturedPopulation.tsx
@@ -12,14 +12,15 @@ import { Trans } from "react-i18next";
 export const SectionFeaturedPopulation = () => {
   return (
     <Box bgGradient="linear-gradient(180deg, #17106e 0%, #2C1FA7 100%)">
-      <Flex
-        position="relative"
-        overflow="hidden"
-        mx={{ base: "-4", lg: "auto" }}
-        px={{ base: "4", lg: "auto" }}
-        direction="row"
-        alignItems="center"
-      >
+      <Container maxW="container.xl" overflowX="hidden">
+        {/* <Flex
+          position="relative"
+          overflow="hidden"
+          mx={{ base: "-4", lg: "auto" }}
+          px={{ base: "4", lg: "auto" }}
+          direction="row"
+          alignItems="center"
+        > */}
         <Box flex="1" position="relative" zIndex="3">
           <Heading mb="6">
             <Trans
@@ -42,21 +43,8 @@ export const SectionFeaturedPopulation = () => {
             />
           </Text>
         </Box>
-        <Box
-          position={{ base: "absolute", lg: "initial" }}
-          right={{ base: "-20", md: "-50%", lg: "auto" }}
-          top={{ base: "50%", lg: "auto" }}
-          transform={{ base: "translateY(-50%)", lg: "initial" }}
-          w={{ base: "200vw", md: "100vw", lg: "auto" }}
-          opacity={{ base: ".8", lg: "auto" }}
-          pointerEvents={{ base: "none", lg: "all" }}
-          flex="2"
-        >
-          {/* <AspectRatio w="full" ratio={{ base: 1, md: 0.8 }}>
-              <AnimationGlobe />
-            </AspectRatio> */}
-        </Box>
-      </Flex>
+        {/* </Flex> */}
+      </Container>
     </Box>
   );
 };

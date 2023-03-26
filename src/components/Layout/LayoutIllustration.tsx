2e99089fbc238a9e00f6e92c2be8c95fd79c2af9
Y0SH4 <yosa.agung13@gmail.com>
2023-03-26 14:46:59 +0700

    refactor fail connect wallet

--- src/components/Layout/LayoutIllustration.tsx
+++ src/components/Layout/LayoutIllustration.tsx
@@ -10,20 +10,35 @@ type Props = {
 export const LayoutIllustration = (props: Props) => {
   const { illustrationUri, title, description, children } = props;
   return (
-    <Container
+    <Box
+      bgGradient="linear-gradient(180deg, #2C1FA7 0%, #6D02C9 100%)"
+      pos="relative"
       display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      maxW="3xl"
-      h="100vh"
     >
-      <Image mb="6" w="64" src={illustrationUri} alt={title} />
-      <Box textAlign="center">
-        <Heading>{title}</Heading>
-        <Text>{description}</Text>
-        {children}
-      </Box>
-    </Container>
+      <Image
+        src="/images/bgHeader_home.png"
+        alt="background"
+        pos="absolute"
+        zIndex={1}
+        objectFit="cover"
+        h="100vh"
+      />
+      <Container
+        zIndex={1000}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        maxW="3xl"
+        h="100vh"
+      >
+        <Image mb="6" w="64" src={illustrationUri} alt={title} />
+        <Box textAlign="center">
+          <Heading>{title}</Heading>
+          <Text>{description}</Text>
+          {children}
+        </Box>
+      </Container>
+    </Box>
   );
 };

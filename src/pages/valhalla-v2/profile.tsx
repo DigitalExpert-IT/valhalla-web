dfdf16d905aee5a03d8782ab3b4fb60e896229e6
Y0SH4 <yosa.agung13@gmail.com>
2023-03-26 14:13:22 +0700

    refactor section my farms

--- src/pages/valhalla-v2/profile.tsx
+++ src/pages/valhalla-v2/profile.tsx
@@ -1,15 +1,15 @@
 import { Box, Image } from "@chakra-ui/react";
 import { LayoutMainV2 } from "components";
-import { LayoutItem } from "components/Layout/LayoutItem";
 import { SectionProfileV2 } from "components/pages/Profile";
+import { SectionMyNFTV2 } from "components/pages/NFTFarm";
 import { withConnection } from "hoc";
 import { composeHoc } from "utils";
 
 const Profile = () => {
   return (
     <LayoutMainV2>
-      <LayoutItem bgGradient="linear(#2C1FA7 50%, #6D02C9 100%)" pt="20">
-        <Box position={"absolute"} w={"full"} pt={"20"} zIndex="0">
+      <Box pt="40">
+        <Box position={"absolute"} w={"full"}>
           <Image
             src="/assets/profile/bg_profile.png"
             alt="matching-image"
@@ -20,7 +20,8 @@ const Profile = () => {
           />
         </Box>
         <SectionProfileV2 />
-      </LayoutItem>
+        <SectionMyNFTV2 />
+      </Box>
     </LayoutMainV2>
   );
 };

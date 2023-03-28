4e5a11c1e51fa0559d64412be0a405f3bb86b395
mananjabrik <manan.jabrik@gmail.com>
2023-03-28 21:08:54 +0700

    fix: rank

--- src/constant/rank.ts
+++ src/constant/rank.ts
@@ -11,9 +11,9 @@ export const rankMap = [
 export const RANK_SYMBOL_MAP = [
   "assets/rank/no-rank.svg",
   "assets/rank/common.svg",
-  "Rare",
-  "Super Rare",
-  "Epic",
-  "Legend",
-  "Super Legend",
+  "assets/rank/rare.svg",
+  "sassets/rank/super-rare.svg",
+  "assets/rank/epic.svg",
+  "assets/rank/legend.svg",
+  "assets/rank/super-legend.svg",
 ];

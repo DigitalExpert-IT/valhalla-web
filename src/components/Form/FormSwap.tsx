bf54281d2db8ee2bafd5b350d84b1a5788b2c0b8
dnfebri <dwinofebri@gmail.com>
2023-03-28 15:19:36 +0700

    refektor swap

--- src/components/Form/FormSwap.tsx
+++ src/components/Form/FormSwap.tsx
@@ -4,9 +4,19 @@ import { useAsyncCall, useSwap } from "hooks";
 import { useTranslation } from "react-i18next";
 import { getGnetRate, getUsdtRate } from "utils";
 import { useEffect, useMemo, useState } from "react";
-import { Box, Button, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { ButtonConnectWrapper } from "components/Button";
 import { FormInput, FormSelect } from "components/FormUtils";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 interface ISwapToken {
   price: string;
@@ -61,61 +71,162 @@ export const FormSwap = () => {
   });
 
   return (
-    <Stack as="form" onSubmit={onSubmit}>
-      <Stack alignItems="center" mb="5">
-        <FormInput
-          control={control}
-          label={t("form.label.from")}
-          name="price"
-          helpertext={t("form.helperText.balance", {
-            balanceOf: symbol
-              ? fromBn(currency.usdt.balance, 6)
-              : fromBn(currency.gnet.balance, 9),
-            symbol: symbol ? "USDT" : "GNET",
-          })}
-          placeholder={"0.0"}
-          type="number"
-          isDisabled={!initialized}
-        />
-        <Stack
-          direction={{ md: "row", sm: "column", base: "column" }}
-          w={"full"}
-        >
-          <Box flex={1}>
-            <FormInput
-              control={control}
-              label={t("form.label.to")}
-              name="amount"
-              helpertext={t("form.helperText.balance", {
+    <SimpleGrid columns={{ base: 1, md: 2 }} gap={{ base: "8", md: "20" }} pos={"relative"}>
+      <Stack as="form" onSubmit={onSubmit}>
+        <Stack alignItems="center" mb="5">
+          <Stack w={"full"}>
+            <Box
+              position={"relative"}
+              border={"1px"}
+              borderRadius={"3xl"}
+              overflow={"hidden"}
+            >
+              <Text
+                position={"absolute"}
+                textAlign={"center"}
+                fontWeight={"bold"}
+                py={"2"}
+                px={"6"}
+                borderRadius={"3xl"}
+                color={"purple.900"}
+                bg={"white"}
+                zIndex={"3"}
+              >
+                {t("form.label.from")}
+              </Text>
+              <FormInput
+                ml={"10"}
+                textAlign="center"
+                textColor={"gray.100"}
+                borderRadius={"3xl"}
+                bg="whiteAlpha.100"
+                _focus={{
+                  border: "none",
+                  bg: "whiteAlpha.200",
+                }}
+                control={control}
+                name="price"
+                placeholder={"0.0"}
+                type="number"
+                isDisabled={!initialized}
+              />
+            </Box>
+            <Text
+              as={"span"}
+              fontSize={"sm"}
+              textAlign={"center"}
+              color={"whiteAlpha.500"}
+            >
+              {t("form.helperText.balance", {
+                balanceOf: symbol
+                  ? fromBn(currency.usdt.balance, 6)
+                  : fromBn(currency.gnet.balance, 9),
+                symbol: symbol ? "USDT" : "GNET",
+              })}
+            </Text>
+          </Stack>
+          <Stack py={"2"} w={"full"}>
+            <SimpleGrid
+              columns={2}
+              placeItems={"center"}
+              bg={"white"}
+              w={"full"}
+              rounded={"3xl"}
+              overflow={"hidden"}
+              textColor={"purple.900"}
+              pos={"relative"}
+            >
+              <Text fontWeight={"bold"}>{t("form.label.swap")}</Text>
+              <Icon pos={"absolute"} zIndex={"3"} fontSize={"xl"}>
+                <AiOutlineArrowRight />
+              </Icon>
+              <FormSelect
+                bg={"white"}
+                control={control}
+                _focus={{
+                  bg: "white",
+                }}
+                // label={t("form.label.swap")}
+                name="currency"
+                option={normalizeCurrencies}
+                isDisabled={!initialized}
+                defaultValue="USDT"
+              />
+            </SimpleGrid>
+          </Stack>
+          <Stack w={"full"} pt={"4"}>
+            <Box
+              position={"relative"}
+              w={"full"}
+              border={"1px"}
+              borderRadius={"3xl"}
+              overflow={"hidden"}
+            >
+              <Text
+                position={"absolute"}
+                textAlign={"center"}
+                fontWeight={"bold"}
+                py={"2"}
+                px={"6"}
+                borderRadius={"3xl"}
+                color={"purple.900"}
+                bg={"white"}
+                zIndex={"3"}
+              >
+                {t("form.label.to")}
+              </Text>
+              <FormInput
+                ml={"10"}
+                textAlign="center"
+                textColor={"gray.100"}
+                borderRadius={"3xl"}
+                bg="whiteAlpha.100"
+                _focus={{
+                  border: "none",
+                  bg: "whiteAlpha.200",
+                }}
+                control={control}
+                name="price"
+                placeholder={"0.0"}
+                type="number"
+                isDisabled={!initialized}
+              />
+            </Box>
+            <Text
+              as={"span"}
+              fontSize={"sm"}
+              color={"whiteAlpha.500"}
+              textAlign={"center"}
+            >
+              {t("form.helperText.balance", {
                 balanceOf: symbol
                   ? fromBn(currency.gnet.balance, 9)
                   : fromBn(currency.usdt.balance, 6),
                 symbol: symbol ? "GNET" : "USDT",
               })}
-              placeholder={"0.0"}
-              type="number"
-              min={0}
-              isDisabled
-              value={price}
-            />
-          </Box>
-          <Box>
-            <FormSelect
-              control={control}
-              label={t("form.label.swap")}
-              name="currency"
-              option={normalizeCurrencies}
-              isDisabled={!initialized}
-              defaultValue="USDT"
-            />
-          </Box>
+            </Text>
+          </Stack>
         </Stack>
+        <ButtonConnectWrapper>
+          <Button type="submit" isLoading={isSwapLoading || !initialized}>
+            {t("common.swap")}
+          </Button>
+        </ButtonConnectWrapper>
       </Stack>
-      <ButtonConnectWrapper>
-        <Button type="submit" isLoading={isSwapLoading || !initialized}>
-          {t("common.swap")}
-        </Button>
-      </ButtonConnectWrapper>
-    </Stack>
+      <Box
+        display={{ base: "none", md: "block" }}
+        pos={"absolute"}
+        right={"0"}
+        left={"0"}
+        my={"8"}
+        w={"0.5"}
+        h={"64"}
+        mx={"auto"}
+        borderRight={"1px"}
+      />
+      <Stack justifyContent={"center"}>
+        <Image src="/assets/SwapImage.png" alt="Image Swap" w={"full"} />
+      </Stack>
+    </SimpleGrid>
   );
 };
---
+++ src/pages/valhalla-v2/swap.tsx
@@ -0,0 +1,69 @@
+import { Card, CardBody, Flex, Heading, Image, Stack } from "@chakra-ui/react";
+import { FormSwap, LayoutMainV2 } from "components";
+import { t } from "i18next";
+
+const Swap = () => {
+  return (
+    <LayoutMainV2>
+      <Flex
+        pos={"relative"}
+        flexDir={"column"}
+        minH={"80vh"}
+        px={"4"}
+        pb={"10"}
+        placeContent={"center"}
+        bgGradient="linear(#2C1FA7 0%, #6D02C9 100%)"
+      >
+        <Heading
+          mt={"24"}
+          textAlign={"center"}
+          _after={{
+            content: `'${t("common.swap").toUpperCase()}'`,
+            alignSelf: "center",
+            display: "block",
+            fontWeight: "black",
+            fontSize: { lg: "180", md: "130", xs: "100", base: "80" },
+            mt: { lg: "-24", md: "-20", xs: "-20", base: "-16" },
+            color: "whiteAlpha.100",
+            textAlign: "center",
+            textTransform: "uppercase",
+          }}
+        >
+          {t("common.swap").toUpperCase()}
+        </Heading>
+        <Card
+          bg={"#6D02C9BF"}
+          mx="auto"
+          py={"8"}
+          w={"full"}
+          px={{ sm: "8" }}
+          rounded={"3xl"}
+          maxW="4xl"
+          zIndex={"3"}
+        >
+          <CardBody>
+            <FormSwap />
+          </CardBody>
+        </Card>
+        <Stack
+          justifyContent={"center"}
+          pos={"absolute"}
+          bottom={"0"}
+          right={"0"}
+          left={"0"}
+          zIndex={"0"}
+        >
+          <Image
+            src="/images/BgSwap.png"
+            alt="Bg Swap"
+            w={"full"}
+            minH={"2xl"}
+            objectFit={"cover"}
+          />
+        </Stack>
+      </Flex>
+    </LayoutMainV2>
+  );
+};
+
+export default Swap;

50472781b257a54d8b4aa119dda0d6cb023fa6df
Dwi Nofebri <dwinofebri@gmail.com>
2023-04-05 12:12:12 +0700

    Refactory: new footer (#163)
    * refactory new footer
    
    * update clean code

---
+++ src/constant/icon.ts
@@ -0,0 +1,7 @@
+export const ICONS_FOOTER = [
+  { href: "#", src: "/assets/icon/instagram.png", alt: "icon instagram" },
+  { href: "#", src: "/assets/icon/twitter.png", alt: "icon twitter" },
+  { href: "#", src: "/assets/icon/facebook.png", alt: "icon facebook" },
+  { href: "#", src: "/assets/icon/tiktok_white.png", alt: "icon tiktok" },
+  { href: "#", src: "/assets/icon/youtube.png", alt: "icon youtube" },
+]

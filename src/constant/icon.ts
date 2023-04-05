5d68c96a1344199fdf44c931fe335656dd8048ac
dnfebri <dwinofebri@gmail.com>
2023-04-05 13:09:03 +0700

    Fix: add link footer

--- src/constant/icon.ts
+++ src/constant/icon.ts
@@ -1,7 +1,27 @@
 export const ICONS_FOOTER = [
-  { href: "#", src: "/assets/icon/instagram.png", alt: "icon instagram" },
-  { href: "#", src: "/assets/icon/twitter.png", alt: "icon twitter" },
-  { href: "#", src: "/assets/icon/facebook.png", alt: "icon facebook" },
-  { href: "#", src: "/assets/icon/tiktok_white.png", alt: "icon tiktok" },
-  { href: "#", src: "/assets/icon/youtube.png", alt: "icon youtube" },
-]
+  {
+    href: "https://www.instagram.com/globalnetworkdefi/",
+    src: "/assets/icon/instagram.png",
+    alt: "icon instagram",
+  },
+  {
+    href: "https://twitter.com/globalnetdefi",
+    src: "/assets/icon/twitter.png",
+    alt: "icon twitter",
+  },
+  {
+    href: "https://www.facebook.com/globalnetworkdefi",
+    src: "/assets/icon/facebook.png",
+    alt: "icon facebook",
+  },
+  {
+    href: "https://www.tiktok.com/@globalnetworkdefi",
+    src: "/assets/icon/tiktok_white.png",
+    alt: "icon tiktok",
+  },
+  {
+    href: "https://www.youtube.com/@globalnetworkdefi",
+    src: "/assets/icon/youtube.png",
+    alt: "icon youtube",
+  },
+];

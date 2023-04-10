e61ba062f5b8054ed782fd6e94f968fa2e6a27cf
Y0SH4 <yosa.agung13@gmail.com>
2023-04-11 02:01:26 +0700

    change rpc url 2nd

--- src/constant/endpoint.ts
+++ src/constant/endpoint.ts
@@ -5,8 +5,7 @@ export const WS_ENDPOINTS = {
 };
 
 export const RPC_ENDPOINTS = {
-  "0x89":
-    "https://matic.getblock.io/c93b3096-bc13-4daf-8c73-b5ac0c1c8855/mainnet/",
+  "0x89": "https://polygon-rpc.com/",
   "0x13881": "https://matic-mumbai.chainstacklabs.com",
   "0x539": "http://localhost:8545",
   "0x29a": "https://valhalacoin.cloud",

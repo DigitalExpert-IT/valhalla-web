4bc2cd9f1a63ea155686921e2160747994b25254
vickoeff <vic.eff3@gmail.com>
2023-05-17 12:20:36 +0700

    create new path downlines api

--- src/pages/api/downlines/[address].ts
+++ src/pages/api/downlines/[address].ts
@@ -4,14 +4,21 @@ import { lowerCase } from "utils";
 
 const prisma = new PrismaClient();
 
+type GroupDownline = {
+  level: number;
+  userList: User[];
+  total: number;
+};
+
 const handler: NextApiHandler = async (req, res) => {
   const address = lowerCase(req.query.address as string);
+  const rank = req.query.rank as string;
   const firstUser = await prisma.user.findUnique({
     where: {
       address,
     },
   });
-  let result: User[] = [
+  let result: (User | GroupDownline)[] = [
     {
       id: "root" as any,
       address,
@@ -20,15 +27,25 @@ const handler: NextApiHandler = async (req, res) => {
       telegramUsername: firstUser?.telegramUsername ?? "",
     },
   ];
+
+  const maxDownline = [0, 10, 20, 40, 60, 80, 100];
   let upperList = [address];
-  for (let i = 0; i < 100; i++) {
+
+  for (let i = 0; i < maxDownline[+rank]; i++) {
     const userList = await prisma.user.findMany({
       where: {
         upline: { in: upperList },
       },
     });
+
+    const group = {
+      level: i + 1,
+      userList: userList,
+      total: userList.length,
+    };
+
     upperList = userList.map(user => user.address);
-    result = [...result, ...userList];
+    result.push(group);
   }
 
   return res.json(result);

003af3dd56c4265f2d948e603b220d88e8668156
mananjabrik <manan.jabrik@gmail.com>
2023-05-17 15:19:38 +0700

    setup template

---
+++ src/constant/pages/dashboard.ts
@@ -0,0 +1,10 @@
+export const DASHBOARD_CATEGORY = [
+  {
+    name: "Menu",
+    menus: [{ name: "Dashboard" }, { name: "Event" }],
+  },
+  {
+    name: "Setting",
+    menus: [{ name: "Instagram" }, { name: "My Collection" }],
+  },
+];
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,17 +1,80 @@
-import { Box, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  Input,
+  ListItem,
+  Stack,
+  Text,
+  UnorderedList,
+} from "@chakra-ui/react";
+import { CopiableText } from "components";
+import { DASHBOARD_CATEGORY } from "constant/pages/dashboard";
+import { useWallet } from "hooks";
+import { jsNumberForAddress } from "react-jazzicon";
+import Jazzicon from "react-jazzicon/dist/Jazzicon";
+import { shortenAddress } from "utils";
 
-const dashboard = () => {
+const UserAddress = () => {
+  const { address } = useWallet();
   return (
-    <Stack direction={"row"} w="full">
-      <Box flex={1}>menu</Box>
-      <Box flex={3}>
-        <FormControl>
-          <Input type="search" />
-        </FormControl>
-      </Box>
-      <Box flex={1}>Account</Box>
+    <Stack spacing="4" direction="row" align="center">
+      <Stack
+        direction="row"
+        spacing="2"
+        align="center"
+        rounded="full"
+        pr="3"
+        h="2rem"
+        overflow="hidden"
+      >
+        <Box mt="1">
+          <Jazzicon diameter={30} seed={jsNumberForAddress(address)} />
+        </Box>
+        <CopiableText fontFamily="mono" display="block" value={address}>
+          {shortenAddress(address)}
+        </CopiableText>
+      </Stack>
     </Stack>
   );
 };
 
-export default dashboard;
+const Dashboard = () => {
+  return (
+    <Stack direction={"row"} spacing={0} w="full" h="100vh">
+      <Stack flex={1}>
+        <UnorderedList>
+          {DASHBOARD_CATEGORY.map((kategoryItem, ctgrIdx) => (
+            <ListItem key={ctgrIdx}>
+              <Text>{kategoryItem.name}</Text>
+              <UnorderedList>
+                {kategoryItem.menus.map((menu, mIdx) => (
+                  <ListItem key={mIdx}>{menu.name}</ListItem>
+                ))}
+              </UnorderedList>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Stack>
+      <Stack direction={"row"} flex={4} bg="gray.100" p="1rem">
+        <Box flex={2}>
+          <FormControl>
+            <Input type="search" bg="white" boxShadow={"md"} />
+          </FormControl>
+          <Box bg="purple.500" p="10" mt="5" rounded="md">
+            <Text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem, molestiae rerum. Eos quod quo sint explicabo unde?
+              Veritatis, vitae. Suscipit cum aut amet saepe maiores fugit in
+              earum doloribus est.
+            </Text>
+          </Box>
+        </Box>
+        <Stack flex={1} alignItems="center" color="gray.800">
+          <UserAddress />
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Dashboard;

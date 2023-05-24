f10fedfadea6f49dea594cc02415be25c7513aba
Ade Yahya <adeyahyaprasetyo@gmail.com>
2023-05-24 22:42:23 +0700

    update

--- src/components/Card/CardProfileV2/CardProfileBonus.tsx
+++ src/components/Card/CardProfileV2/CardProfileBonus.tsx
@@ -4,19 +4,27 @@ import {
   WidgetProfileBtn,
 } from "components/Widget/WidgetProfile";
 import { useAsyncCall, useValhalla } from "hooks";
+import { useContractWrite } from "@thirdweb-dev/react";
 import { t } from "i18next";
 import { CardProfileV2 } from "./CardProfileV2";
 import { fromBn } from "evm-bn";
 import { useRewardMap } from "hooks/valhalla/useRewardMap";
 import { useRankReward } from "hooks/valhalla/useRankReward";
+import { useValhallaContract } from "hooks/useValhallaContract";
 
 export const CardProfileBonus = () => {
-  const { claimReward, claimRankReward, globalPool, isRankRewardClaimable } =
-    useValhalla();
+  const { claimRankReward, globalPool, isRankRewardClaimable } = useValhalla();
   const rewardMap = useRewardMap();
   const rankReward = useRankReward();
-  const claimRewardAsync = useAsyncCall(claimReward);
-  const claimRankRewardAsync = useAsyncCall(claimRankReward);
+  const valhalla = useValhallaContract();
+  const claimReward = useContractWrite(valhalla.contract, "claimReward");
+  const claimRewardAsync = useAsyncCall(claimReward.mutateAsync);
+
+  const handleClaimReward = async () => {
+    await claimRewardAsync.exec({ args: [] });
+    await rewardMap.refetch();
+  };
+
   return (
     <CardProfileV2>
       <Stack gap={"3"} px={"4"} mx={"auto"} w={"full"} maxW={"2xl"}>
@@ -33,7 +41,7 @@ export const CardProfileBonus = () => {
               <Text>{rewardMap.data && fromBn(rewardMap.data)} MATIC</Text>
             </Stack>
             <WidgetProfileBtn
-              onClick={claimRewardAsync.exec}
+              onClick={handleClaimReward}
               isLoading={claimRewardAsync.isLoading}
             >
               {t("common.claim")}
@@ -47,8 +55,8 @@ export const CardProfileBonus = () => {
               <Text>{rankReward.data && fromBn(rankReward.data)} MATIC</Text>
             </Stack>
             <WidgetProfileBtn
-              onClick={claimRankRewardAsync.exec}
-              isLoading={claimRankRewardAsync.isLoading}
+              // onClick={claimRankRewardAsync.exec}
+              // isLoading={claimRankRewardAsync.isLoading}
               isDisabled={!isRankRewardClaimable}
             >
               {t("common.claim")}

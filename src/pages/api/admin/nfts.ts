be1cf84d83772029b377858140c376966ea5b3df
mananjabrik <manan.jabrik@gmail.com>
2023-06-06 23:42:51 +0700

    update

--- src/pages/api/admin/nfts.ts
+++ src/pages/api/admin/nfts.ts
@@ -4,27 +4,27 @@ import { INFTItem, getNFTsByTypeInRow, getTotalPagesNFTByType } from "utils";
 const handler: NextApiHandler = async (req, res) => {
   const { type, page, limit } = req.query;
 
-  if (!type)
-    res.status(400).json({
-      status: "bad request",
-      code: "400",
-      message: ["need query type"],
-    });
-
   const isLimitNumOrNan = Number(limit) < 1 || !Number(limit);
   const isTakeNumOrNan = Number(page) < 1 || !Number(page);
 
   const take = isLimitNumOrNan ? 10 : Number(limit);
   const skip = take * (isTakeNumOrNan ? 0 : Number(page) - 1);
 
-  const NFTs: INFTItem[] = await getNFTsByTypeInRow(String(type), skip, take);
-  const totalNfts = await getTotalPagesNFTByType(String(type), take);
+  const NFTs: INFTItem[] = await getNFTsByTypeInRow(
+    String(!type ? 0 : type),
+    skip,
+    take
+  );
+  const totalNfts = await getTotalPagesNFTByType(
+    String(!type ? 0 : type),
+    take
+  );
 
   return res.status(200).json({
     totalItemPerPage: NFTs.length,
     totalPage: totalNfts?.totalPage,
     totalData: totalNfts?.totalData,
-    farm: !type ? 0 : type,
+    cardType: !type ? 0 : type,
     data: NFTs,
   });
 };

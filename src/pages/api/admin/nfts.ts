1b2af2701ad56af946db88427b2d1149e0e45e3d
mananjabrik <manan.jabrik@gmail.com>
2023-06-06 23:39:06 +0700

    update

--- src/pages/api/admin/nfts.ts
+++ src/pages/api/admin/nfts.ts
@@ -1,10 +1,32 @@
-import { PrismaClient, User } from "@prisma/client";
 import { NextApiHandler } from "next";
-
-const prisma = new PrismaClient();
+import { INFTItem, getNFTsByTypeInRow, getTotalPagesNFTByType } from "utils";
 
 const handler: NextApiHandler = async (req, res) => {
-  return res.status(200).json("OK");
+  const { type, page, limit } = req.query;
+
+  if (!type)
+    res.status(400).json({
+      status: "bad request",
+      code: "400",
+      message: ["need query type"],
+    });
+
+  const isLimitNumOrNan = Number(limit) < 1 || !Number(limit);
+  const isTakeNumOrNan = Number(page) < 1 || !Number(page);
+
+  const take = isLimitNumOrNan ? 10 : Number(limit);
+  const skip = take * (isTakeNumOrNan ? 0 : Number(page) - 1);
+
+  const NFTs: INFTItem[] = await getNFTsByTypeInRow(String(type), skip, take);
+  const totalNfts = await getTotalPagesNFTByType(String(type), take);
+
+  return res.status(200).json({
+    totalItemPerPage: NFTs.length,
+    totalPage: totalNfts?.totalPage,
+    totalData: totalNfts?.totalData,
+    farm: !type ? 0 : type,
+    data: NFTs,
+  });
 };
 
 export default handler;
--- src/utils/query.ts
+++ src/utils/query.ts
@@ -50,3 +50,56 @@ export const getNFTByAddress = async (address: string) => {
 
   return nftList;
 };
+
+export const getTotalPagesNFTByType = async (cardId: string, take: number) => {
+  const totalNFT: { totalPage: number; totalData: number }[] =
+    await prisma.$queryRaw`
+    SELECT CEIL(CAST(COUNT(*)  as float) / ${take}) as "totalPage", CAST(COUNT(*) as int) as "totalData" from (
+      SELECT distinct on ("tokenId") "tokenId", * from (
+        SELECT 
+          "Event"."id",
+          "Event"."address",
+          "Event"."transactionHash",
+          "Event"."args"->>'tokenId' as "tokenId",
+          "Event"."args"->>'from' as "from",
+          "Event"."args"->>'to' as "to",
+          "Event"."blockNumber",
+          cast("NftMetadata"."mintingPrice"/1e9 as int) as "price",
+          cast(cast("NftMetadata"."farmPercentage" as DECIMAL)/10 as float) as "farmPercentage",
+          "NftMetadata"."mintedAt" as "mintedAt",  "NftMetadata"."isBlackListed" as "isBlackListed",
+          "NftMetadata"."cardId" as "cardId"
+        from "Event" INNER JOIN "NftMetadata" ON "Event"."args"->>'tokenId'="NftMetadata"."tokenId"
+      ) "transList" order by "transList"."tokenId", "transList"."blockNumber" DESC
+    ) "filteredTransList" where "cardId"=${cardId}
+  `;
+
+  return totalNFT.at(0);
+};
+
+export const getNFTsByTypeInRow = async (
+  cardId: string,
+  skip: number,
+  take: number
+) => {
+  const nftList: INFTItem[] = await prisma.$queryRaw`
+        SELECT * FROM (SELECT * from (
+          SELECT distinct on ("tokenId") "tokenId", * from (
+            SELECT 
+              "Event"."id",
+              "Event"."address",
+              "Event"."transactionHash",
+              "Event"."args"->>'tokenId' as "tokenId",
+              "Event"."args"->>'from' as "from",
+              "Event"."args"->>'to' as "to",
+              "Event"."blockNumber",
+              cast("NftMetadata"."mintingPrice"/1e9 as int) as "price",
+              cast(cast("NftMetadata"."farmPercentage" as DECIMAL)/10 as float) as "farmPercentage",
+              "NftMetadata"."mintedAt" as "mintedAt",  "NftMetadata"."isBlackListed" as "isBlackListed",
+              "NftMetadata"."cardId" as "cardId"
+            from "Event" INNER JOIN "NftMetadata" ON "Event"."args"->>'tokenId'="NftMetadata"."tokenId"
+          ) "transList" order by "transList"."tokenId", "transList"."blockNumber" DESC
+        ) "filteredTransList" where "filteredTransList"."cardId"=${cardId}) 
+        "pagination" OFFSET ${skip} ROWS FETCH NEXT ${take} ROWS ONLY `;
+
+  return nftList;
+};

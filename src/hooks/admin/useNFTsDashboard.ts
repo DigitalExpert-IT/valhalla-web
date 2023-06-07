bffa7e5882e0094c653cf070213489fec23155b7
mananjabrik <manan.jabrik@gmail.com>
2023-06-07 14:26:57 +0700

    update commant

--- src/hooks/admin/useNFTsDashboard.ts
+++ src/hooks/admin/useNFTsDashboard.ts
@@ -1,6 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import Axios from "axios";
 
+/**
+ *
+ * @param type card type (1-5)
+ * @param page page (1-total-page)
+ * @param limit limit item
+ * @returns Query Result
+ *
+ * @example ``` useNFTsDashboard(1, 2, 10) ```
+ */
+
 export const useNFTsDashboard = (type: number, page: number, limit: number) => {
   return useQuery(["NFTs", type, page, limit], async () => {
     const axiosResponse = await Axios.get(
--- src/hooks/admin/useUsersDashboard.ts
+++ src/hooks/admin/useUsersDashboard.ts
@@ -2,6 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import Axios from "axios";
 import { IUser } from "pages/api/admin/user";
 
+/**
+ *
+ * @param page page number
+ * @param limit limit item
+ * @returns Query Result
+ *
+ * @example ```useUsersDasboard(1, 10)```
+ */
+
 export const useUsersDasboard = (page: number, limit: number) => {
   return useQuery(["Users", page, limit], async () => {
     const axiosResponse = await Axios.get<IUser>(

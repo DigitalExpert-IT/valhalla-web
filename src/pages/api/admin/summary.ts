5c9601af82679ef13c3cada3ae31ff6fdec5ca88
mananjabrik <manan.jabrik@gmail.com>
2023-06-15 02:06:10 +0700

    update

--- src/pages/api/admin/summary.ts
+++ src/pages/api/admin/summary.ts
@@ -18,47 +18,45 @@ interface ISummaryDashboard {
 // const prisma = new PrismaClient();
 
 const handler: NextApiHandler = async (req, res) => {
-  try {
-    const nfts = await getSummary(
-      new Date("2023-05-24T09:33:42.000Z"),
-      new Date("2023-05-25T09:33:42.000Z")
-    );
+  const nfts = await getSummary(
+    new Date("2023-05-24T09:33:42.000Z"),
+    new Date("2023-05-24T20:33:42.000Z")
+  );
 
-    const summary = nfts.reduce(
-      (acc, item) => {
-        const addBlacklist = item.isBlackListed
-          ? acc.totalBlackListed + 1
-          : acc.totalBlackListed;
+  const summary = nfts.reduce(
+    (acc, item) => {
+      const addBlacklist = item.isBlackListed
+        ? acc.totalBlacklist + 1
+        : acc.totalBlacklist;
 
-        const addActive = !item.isBlackListed
-          ? acc.totalActive + 1
-          : acc.totalActive;
+      const addActive = !item.isBlackListed
+        ? acc.totalActive + 1
+        : acc.totalActive;
 
-        const rewardInSec = item.baseReward / 86400;
-        const lastFarmNum = +new Date(item.lastFarm);
-        const minttedNum = +new Date(item.mintedAt);
-        const claimReward = item.baseReward * 450;
-        const nftClaim = (lastFarmNum - minttedNum) * rewardInSec;
+      const rewardInSec = item.baseReward / 86400;
+      const lastFarmNum = +new Date(item.lastFarm);
+      const minttedNum = +new Date(item.mintedAt);
+      const claimReward = item.baseReward * 450;
+      const nftClaim = (lastFarmNum - minttedNum) * rewardInSec;
 
-        return {
-          totalProfite: acc.totalProfite + claimReward,
-          totalActive: addActive,
-          totalBlackListed: addBlacklist,
-          totalClaimedNft: acc.totalClaimedNft + nftClaim,
-        };
-      },
-      {
-        totalProfite: 0,
-        totalBlackListed: 0,
-        totalActive: 0,
-        totalClaimedNft: 0,
-      }
-    );
+      return {
+        NFTOnUser: acc.NFTOnUser + 1,
+        totalProfite: acc.totalProfite + claimReward,
+        totalActive: addActive,
+        totalBlacklist: addBlacklist,
+        totalClaimedNft: acc.totalClaimedNft + nftClaim,
+      };
+    },
+    {
+      NFTOnUser: 0,
+      totalProfite: 0,
+      totalBlacklist: 0,
+      totalActive: 0,
+      totalClaimedNft: 0,
+    }
+  );
 
-    return res.json(summary);
-  } catch (e: any) {
-    res.status(500).json(e.message);
-  }
+  return res.json(summary);
 };
 
 export default handler;

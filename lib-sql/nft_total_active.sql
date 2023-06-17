3b97b82688b26a7c158bd8a6616cc26b926907ab
mananjabrik <manan.jabrik@gmail.com>
2023-06-17 17:09:37 +0700

    format sql easy read

--- lib-sql/nft_total_active.sql
+++ lib-sql/nft_total_active.sql
@@ -1,19 +1,37 @@
- SELECT 
-      CAST(SUM((("price" * "farmPercentage") / 100) * 450)as int) as "totalProfit", CAST(COUNT(*) as int) as "totalActiveNFT"
-      from (
-        SELECT distinct on ("tokenId") "tokenId", * from (
-          SELECT 
-            "Event"."id",
-            "Event"."address",
-            "Event"."transactionHash",
-            "Event"."args"->>'tokenId' as "tokenId",
-            "Event"."args"->>'from' as "from",
-            "Event"."args"->>'to' as "to",
-            "Event"."blockNumber",
-            cast("NftMetadata"."mintingPrice"/1e9 as int) as "price",
-            cast(cast("NftMetadata"."farmPercentage" as DECIMAL)/10 as float) as "farmPercentage",
-            "NftMetadata"."mintedAt" as "mintedAt",  "NftMetadata"."isBlackListed" as "isBlackListed",
-            "NftMetadata"."cardId" as "cardId"
-          from "Event" INNER JOIN "NftMetadata" ON "Event"."args"->>'tokenId'="NftMetadata"."tokenId"
-        ) "transList" order by "transList"."tokenId", "transList"."blockNumber" DESC
-    ) "filteredTransList" where "isBlackListed"=false
+SELECT
+  CAST(
+    SUM((("price" * "farmPercentage") / 100) * 450) as int
+  ) as "totalProfit",
+  CAST(COUNT(*) as int) as "totalActiveNFT"
+from
+  (
+    SELECT
+      distinct on ("tokenId") "tokenId",
+      *
+    from
+      (
+        SELECT
+          "Event"."id",
+          "Event"."address",
+          "Event"."transactionHash",
+          "Event"."args" ->> 'tokenId' as "tokenId",
+          "Event"."args" ->> 'from' as "from",
+          "Event"."args" ->> 'to' as "to",
+          "Event"."blockNumber",
+          cast("NftMetadata"."mintingPrice" / 1e9 as int) as "price",
+          cast(
+            cast("NftMetadata"."farmPercentage" as DECIMAL) / 10 as float
+          ) as "farmPercentage",
+          "NftMetadata"."mintedAt" as "mintedAt",
+          "NftMetadata"."isBlackListed" as "isBlackListed",
+          "NftMetadata"."cardId" as "cardId"
+        from
+          "Event"
+          INNER JOIN "NftMetadata" ON "Event"."args" ->> 'tokenId' = "NftMetadata"."tokenId"
+      ) "transList"
+    order by
+      "transList"."tokenId",
+      "transList"."blockNumber" DESC
+  ) "filteredTransList"
+where
+  "isBlackListed" = false
--- lib-sql/user_nft_pagination.sql
+++ lib-sql/user_nft_pagination.sql
@@ -2,13 +2,13 @@ SELECT
 	"User"."address",
 	"User"."upline",
 	"User"."rank",
-	SUM(cast("NftDetail" ->>'maxReward' AS int)) AS "profit",
+	SUM(cast("NftDetail" ->> 'maxReward' AS int)) AS "profit",
 	json_agg("NftDetail") AS "NftList",
 	COUNT("NftDetail") AS "TotalNFT",
 	SUM(CAST("NftDetail" ->> 'price' AS INTEGER)) AS "totalInvest"
 FROM
-	"User"
-	FULL JOIN (
+	"User" FULL
+	JOIN (
 		SELECT
 			"User"."address",
 			"User"."rank",
@@ -20,46 +20,66 @@ FROM
 			"farmPercentage",
 			"mintedAt",
 			json_build_object(
-				'tokenId', "args" ->> 'tokenId', 
-				'price', "newEvent"."price", 
-				'isBlackListed', "newEvent"."isBlackListed", 
-				'percentage', "newEvent"."farmPercentage", 
-				'mintedAt', "newEvent"."mintedAt", 
-				'cardId', "newEvent"."cardId", 
-				'from', "from", 
-				'rewardPerDay', cast(cast("price" * "farmPercentage" as DEC) / 100 as int),
-				'maxReward', cast(cast("price" * "farmPercentage" as DEC) / 100 as int) * 450
+				'tokenId',
+				"args" ->> 'tokenId',
+				'price',
+				"newEvent"."price",
+				'isBlackListed',
+				"newEvent"."isBlackListed",
+				'percentage',
+				"newEvent"."farmPercentage",
+				'mintedAt',
+				"newEvent"."mintedAt",
+				'cardId',
+				"newEvent"."cardId",
+				'from',
+				"from",
+				'rewardPerDay',
+				cast(
+					cast("price" * "farmPercentage" as DEC) / 100 as int
+				),
+				'maxReward',
+				cast(
+					cast("price" * "farmPercentage" as DEC) / 100 as int
+				) * 450
 			) AS "NftDetail"
 		FROM
 			"User"
-			INNER JOIN ( SELECT DISTINCT ON ("tokenId")
-					"tokenId",
+			INNER JOIN (
+				SELECT
+					DISTINCT ON ("tokenId") "tokenId",
 					*
-				FROM (
-					SELECT
-						"Event"."id",
-						"Event"."address",
-						"Event"."transactionHash",
-						"Event"."args" ->> 'tokenId' AS "tokenId",
-						"Event"."args" ->> 'from' AS "from",
-						"Event"."args" ->> 'to' AS "to",
-						"Event"."blockNumber",
-						"Event"."args",
-						cast("NftMetadata"."mintingPrice" / 1e9 AS int) AS "price",
-						cast(cast("NftMetadata"."farmPercentage" AS DECIMAL) / 10 AS float) AS "farmPercentage",
-						"NftMetadata"."mintedAt" AS "mintedAt",
-						"NftMetadata"."isBlackListed" AS "isBlackListed",
-						"NftMetadata"."cardId" AS "cardId"
-					FROM
-						"Event"
-						INNER JOIN "NftMetadata" ON "Event"."args" ->> 'tokenId' = "NftMetadata"."tokenId") "transList"
+				FROM
+					(
+						SELECT
+							"Event"."id",
+							"Event"."address",
+							"Event"."transactionHash",
+							"Event"."args" ->> 'tokenId' AS "tokenId",
+							"Event"."args" ->> 'from' AS "from",
+							"Event"."args" ->> 'to' AS "to",
+							"Event"."blockNumber",
+							"Event"."args",
+							cast("NftMetadata"."mintingPrice" / 1e9 AS int) AS "price",
+							cast(
+								cast("NftMetadata"."farmPercentage" AS DECIMAL) / 10 AS float
+							) AS "farmPercentage",
+							"NftMetadata"."mintedAt" AS "mintedAt",
+							"NftMetadata"."isBlackListed" AS "isBlackListed",
+							"NftMetadata"."cardId" AS "cardId"
+						FROM
+							"Event"
+							INNER JOIN "NftMetadata" ON "Event"."args" ->> 'tokenId' = "NftMetadata"."tokenId"
+					) "transList"
 				ORDER BY
 					"transList"."tokenId",
-					"transList"."blockNumber" DESC) "newEvent" ON "User"."address" = "newEvent"."args" ->> 'to') "filterEvent" ON "User"."address" = "filterEvent"."to"
+					"transList"."blockNumber" DESC
+			) "newEvent" ON "User"."address" = "newEvent"."args" ->> 'to'
+	) "filterEvent" ON "User"."address" = "filterEvent"."to"
 GROUP BY
 	"User"."address",
 	"User"."rank",
 	"price",
 	"User"."upline"
-ORDER BY "profit" DESC
-OFFSET 1183 ROWS FETCH NEXT 10 ROWS ONLY;
+ORDER BY
+	"profit" DESC OFFSET 1183 ROWS FETCH NEXT 10 ROWS ONLY;

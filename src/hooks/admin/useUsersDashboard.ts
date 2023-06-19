b6262a5678567812ac45a9614f8ec2866b44a025
mananjabrik <manan.jabrik@gmail.com>
2023-06-19 19:33:19 +0700

    update

--- src/hooks/admin/useUsersDashboard.ts
+++ src/hooks/admin/useUsersDashboard.ts
@@ -9,17 +9,21 @@ import { IAdminDashboard } from "pages/api/admin/user";
  * @returns Query Result
  *
  * @example ```useUsersDasboard(1, 10)```
+ * if u wanna sort by best profit, here the option just past DESC on third parameter,
+ * but if u wanna filter more spesific, u can use option address or rank
  */
 
 export const useUsersDasboard = (
   page: number,
   limit: number,
   orderBy?: string,
-  option?: { address: string; rank: string }
+  option?: { address?: string; rank?: string }
 ) => {
   return useQuery(["Users", page, limit, orderBy, option], async () => {
     const axiosResponse = await Axios.post<IAdminDashboard>(
-      `/api/admin/user?page=${page}&limit=${limit}&orderBy=${orderBy}`,
+      `/api/admin/user?page=${page}&limit=${limit}${
+        orderBy && `orderBy=${orderBy}`
+      }`,
       {
         data: {
           option,

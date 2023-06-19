620d82e6b21400deb4d448c446e48e467bf8bb3e
mananjabrik <manan.jabrik@gmail.com>
2023-06-19 11:42:49 +0700

    new receipt

--- lib-sql/user_with_nft_pagination.sql
+++ lib-sql/user_with_nft_pagination.sql
@@ -1,85 +1,56 @@
 SELECT
 	"User"."address",
-	"User"."upline",
-	"User"."rank",
-	SUM(cast("NftDetail" ->> 'maxReward' AS int)) AS "profit",
-	json_agg("NftDetail") AS "NftList",
-	COUNT("NftDetail") AS "TotalNFT",
-	SUM(CAST("NftDetail" ->> 'price' AS INTEGER)) AS "totalInvest"
-FROM
-	"User" FULL
-	JOIN (
+	"upline",
+	"rank",
+	"telegramUsername",
+	json_agg("NFT"."nftDetail"),
+	cast(COUNT("NFT"."nftDetail") as int) "totalNft"
+from
+	"User"
+	LEFT JOIN (
 		SELECT
-			"User"."address",
-			"User"."rank",
-			"User"."upline",
-			"User"."telegramUsername",
-			"from",
-			"to",
-			"price",
-			"farmPercentage",
-			"mintedAt",
-			json_build_object(
-				'tokenId',
-				"args" ->> 'tokenId',
-				'price',
-				"newEvent"."price",
-				'isBlackListed',
-				"newEvent"."isBlackListed",
-				'percentage',
-				"newEvent"."farmPercentage",
-				'mintedAt',
-				"newEvent"."mintedAt",
-				'cardId',
-				"newEvent"."cardId",
-				'from',
-				"from",
-				'rewardPerDay',
-				cast(
-					cast("price" * "farmPercentage" AS DEC) / 100 AS int
-				),
-				'maxReward',
-				cast(
-					cast("price" * "farmPercentage" AS DEC) / 100 AS int
-				) * 450
-			) AS "NftDetail"
-		FROM
-			"User"
-			INNER JOIN (
+			distinct on ("tokenId") "tokenId",
+			*
+		from
+			(
 				SELECT
-					DISTINCT ON ("tokenId") "tokenId",
-					*
-				FROM
-					(
-						SELECT
-							"Event"."id",
-							"Event"."address",
-							"Event"."transactionHash",
-							"Event"."args" ->> 'tokenId' AS "tokenId",
-							"Event"."args" ->> 'from' AS "from",
-							"Event"."args" ->> 'to' AS "to",
-							"Event"."blockNumber",
-							"Event"."args",
-							cast("NftMetadata"."mintingPrice" / 1e9 AS int) AS "price",
-							cast(
-								cast("NftMetadata"."farmPercentage" AS DECIMAL) / 10 AS float
-							) AS "farmPercentage",
-							"NftMetadata"."mintedAt" AS "mintedAt",
-							"NftMetadata"."isBlackListed" AS "isBlackListed",
-							"NftMetadata"."cardId" AS "cardId"
-						FROM
-							"Event"
-							INNER JOIN "NftMetadata" ON "Event"."args" ->> 'tokenId' = "NftMetadata"."tokenId"
-					) "transList"
-				ORDER BY
-					"transList"."tokenId",
-					"transList"."blockNumber" DESC
-			) "newEvent" ON "User"."address" = "newEvent"."args" ->> 'to'
-	) "filterEvent" ON "User"."address" = "filterEvent"."to"
+					"Event"."id",
+					"Event"."address",
+					"Event"."transactionHash",
+					"Event"."args" ->> 'tokenId' as "tokenId",
+					"Event"."args" ->> 'from' as "from",
+					"Event"."args" ->> 'to' as "to",
+					"Event"."blockNumber",
+					json_build_object(
+						'tokenId',
+						"args" ->> 'tokenId',
+						'price',
+						cast("NftMetadata"."mintingPrice" / 1e9 as int),
+						'farmPercentage',
+						cast(
+							cast("NftMetadata"."farmPercentage" as DECIMAL) / 10 as float
+						),
+						'mintedAt',
+						"mintedAt",
+						'cardId',
+						"cardId",
+						'from',
+						"args" ->> 'from',
+						'to',
+						"args" ->> 'to',
+						'blockNumber',
+						"Event"."blockNumber"
+					) as "nftDetail"
+				from
+					"Event"
+					INNER JOIN "NftMetadata" ON "Event"."args" ->> 'tokenId' = "NftMetadata"."tokenId"
+			) "transList"
+		order by
+			"transList"."tokenId",
+			"transList"."blockNumber" desc
+	) "NFT" ON "NFT"."to" = "User"."address"
 GROUP BY
 	"User"."address",
-	"User"."rank",
-	"price",
-	"User"."upline"
-ORDER BY
-	"profit" DESC NULLS LAST OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY;
+	"upline",
+	"rank",
+	"telegramUsername"

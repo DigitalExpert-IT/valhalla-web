e01763806b91b9541e5306ae7831fe0c798c2c08
mananjabrik <manan.jabrik@gmail.com>
2023-07-05 00:34:00 +0700

    re adjust hook

--- src/hooks/user/dashboard/useListDownlines.ts
+++ src/hooks/user/dashboard/useListDownlines.ts
@@ -5,12 +5,24 @@ export const useListDownlines = (
   address: string,
   page: number,
   limit: number,
-  level: number
+  orderBy?: string,
+  filter?: { level: string; rank?: string }
 ) => {
-  return useQuery(["downlines", { page, limit, level }], async () => {
-    const axiosResponse = await Axios.get(
-      `/api/downlines/v2/tree/list-user/${address}?page=${page}&limit=${limit}&level=${level}`
-    );
-    return axiosResponse.data;
-  });
+  return useQuery(
+    [
+      "downlines",
+      { page, limit, orderBy, level: filter?.level, rank: filter?.rank },
+    ],
+    async () => {
+      const axiosResponse = await Axios.post(
+        `/api/downlines/v2/tree/list-user/${address}?page=${page}&limit=${limit}${
+          orderBy && `&orderBy=${orderBy}`
+        }`,
+        {
+          ...filter,
+        }
+      );
+      return axiosResponse.data;
+    }
+  );
 };

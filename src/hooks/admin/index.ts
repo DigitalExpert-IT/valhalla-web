005912eb9e85f2c513ec0d86e94d20789b5f27d4
mananjabrik <manan.jabrik@gmail.com>
2023-07-07 12:44:00 +0700

    update

--- src/hooks/admin/index.ts
+++ src/hooks/admin/index.ts
@@ -2,6 +2,6 @@ export * from "./useBasicDashboardInfo";
 export * from "./useDefaultAdminRole";
 export * from "./useHasRoleAdmin";
 export * from "./useListNFTsDashboardByType";
-export * from "./useListUserNFTsDashboardByType";
 export * from "./useSummary";
 export * from "./useUsersDashboard";
+export * from "./useListUserNFTsDaahboardByType";

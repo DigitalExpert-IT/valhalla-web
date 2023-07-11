d81012cac12b52a511dee71a57cc706dd567c172
deddybrama11 <dedybramayadi@gmail.com>
2023-07-11 21:44:51 +0700

    Test json metadata standard erc721 (2)

--- src/pages/api/image/genesis/[id].ts
+++ src/pages/api/image/genesis/[id].ts
@@ -11,7 +11,8 @@ const handler: NextApiHandler = async (req, res) => {
   return res.json({
     name: "NFT Genesis",
     description: "automatically receive a generous 2% share of the marketing revenues generated from subsequent NFT Farm sales.",
-    video: image,
+    image: "https://globalnetwork.finance/images/nft-genesis.png",
+    animation_url: image,
   });
 };
 

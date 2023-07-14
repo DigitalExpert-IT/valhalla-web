ce24fb3486487f2e8ee1326cbe39a0aec5d2a17a
mananjabrik <manan.jabrik@gmail.com>
2023-07-14 10:20:37 +0700

    update hook available find  downlines

--- src/hooks/user/dashboard/useListDownlines.ts
+++ src/hooks/user/dashboard/useListDownlines.ts
@@ -1,12 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import Axios from "axios";
+import { IAdminDashboard } from "interface";
+
+/**
+ *
+ * @param address address cape or u can call top root
+ * @param page page active
+ * @param limit limit perPage
+ * @param orderBy short by potential profit, u just have to option "ASC" OR "DESC"
+ * @param filter  type object for filtering by level, rank, or u can specially find downlines
+ * @returns axios response data
+ *
+ */
+
+interface ITreeDownlines extends IAdminDashboard {
+  level: number;
+}
 
 export const useListDownlines = (
   address: string,
   page: number,
   limit: number,
-  orderBy?: string,
-  filter?: { level: string; rank?: string }
+  orderBy?: string | "asc" | "desc",
+  filter?: { level: string; rank?: string; downlines: string }
 ) => {
   return useQuery(
     [
@@ -14,7 +30,7 @@ export const useListDownlines = (
       { page, limit, orderBy, level: filter?.level, rank: filter?.rank },
     ],
     async () => {
-      const axiosResponse = await Axios.post(
+      const axiosResponse = await Axios.post<ITreeDownlines>(
         `/api/downlines/v2/tree/list-user/${address}?page=${page}&limit=${limit}${
           orderBy && `&orderBy=${orderBy}`
         }`,
@@ -22,7 +38,7 @@ export const useListDownlines = (
           ...filter,
         }
       );
-      return axiosResponse.data;
+      return axiosResponse.data as ITreeDownlines;
     }
   );
 };

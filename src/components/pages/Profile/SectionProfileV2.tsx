01d00152285a4576b8779893298aa1a25aaf61a6
vickoeff <vic.eff3@gmail.com>
2023-07-17 13:44:20 +0700

    fix profile widget profile value

--- src/components/pages/Profile/SectionProfileV2.tsx
+++ src/components/pages/Profile/SectionProfileV2.tsx
@@ -155,7 +155,7 @@ export const SectionProfileV2 = () => {
         <WidgetProfileMember
           label={"common.totalPotentialProfit"}
           isLoading={summaryLoading}
-          value={`${summaryData?.totalPotentialProfit ?? 0} GNET`}
+          value={String(summaryData?.totalPotentialProfit) ?? "0"}
         />
         <WidgetProfileMember
           isLoading={

7cb4c728e4c1edd6ff5b20876ab2d877e7f11620
Y0SH4 <yosa.agung13@gmail.com>
2023-07-28 19:51:45 +0700

    fix error

---
+++ src/constant/unitList.ts
@@ -0,0 +1,7 @@
+export const UNIT_LIST = [
+  { suffix: "T", threshold: 1e12 },
+  { suffix: "B", threshold: 1e9 },
+  { suffix: "M", threshold: 1e6 },
+  { suffix: "K", threshold: 1e3 },
+  { suffix: "", threshold: 1 },
+];
--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,30 +1,21 @@
-import { BigNumber, BigNumberish, utils } from "ethers/lib/ethers";
+import { BigNumber } from "ethers/lib/ethers";
+import { UNIT_LIST } from "constant/unitList";
 import { fromBn } from "evm-bn";
 
 const toFixed = (num: number, symbol = "") => {
-  const formatted = num
-    .toFixed(3)
-    .replace(/\.000$/, "")
-    .replace(/00$/, "");
-  return `${formatted}${symbol}`;
+  if (symbol === "K") {
+    if (num % 1 === 0) return `${num}000`;
+    return `${num.toFixed(2)}K`;
+  }
+  return `${num}${symbol}`;
 };
 
 export const prettyBn = (bn?: BigNumber, baseNumber = 18): string => {
   if (!bn) return "0";
   const value = +fromBn(bn, baseNumber);
   if (value === 0) return "0";
-
-  if (value > 1_000_000_000) {
-    return toFixed(value / 1_000_000_000, "B");
-  }
-
-  if (value > 1_000_000) {
-    return toFixed(value / 1_000_000, "M");
-  }
-
-  if (value > 1_000) {
-    return toFixed(value / 1_0, "000");
-  }
-
-  return toFixed(value);
+  const converter = UNIT_LIST.find(x => Math.abs(value) >= x.threshold);
+  return converter !== undefined
+    ? toFixed(value / converter!.threshold, converter!.suffix)
+    : toFixed(value);
 };

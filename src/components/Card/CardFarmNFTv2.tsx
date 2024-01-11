22f912a922f6420e51e77062eb9791e7ad047ee1
Julius <juliusagung123@gmail.com>
2024-01-11 16:46:56 +0700

    feat: add badge promote ETH on nft farm

--- src/components/Card/CardFarmNFTv2.tsx
+++ src/components/Card/CardFarmNFTv2.tsx
@@ -41,8 +41,35 @@ export const CardFarmNFTV2: React.FC<CardNFTV2Props> = props => {
           rounded="xl"
         >
           <Stack>
-            <Box as="video" autoPlay loop muted rounded="xl">
-              <source src={CARD_IMAGE_MAP[props.id as "0"]} type="video/mp4" />
+            <Box pos={"relative"} overflow={"hidden"}>
+              <Box
+                pos={"absolute"}
+                width={"fit-content"}
+                height={"fit-content"}
+                borderRadius={2}
+                top={8}
+                left={-16}
+                px={0}
+                py={0}
+                background={"red"}
+                transform={"rotate(315deg);"}
+              >
+                <Text
+                  fontSize={"15px"}
+                  fontWeight={"bold"}
+                  color={"white"}
+                  textTransform={"uppercase"}
+                  px={"4rem"}
+                >
+                  Promote ETF
+                </Text>
+              </Box>
+              <Box as="video" autoPlay loop muted rounded="xl">
+                <source
+                  src={CARD_IMAGE_MAP[props.id as "0"]}
+                  type="video/mp4"
+                />
+              </Box>
             </Box>
             <Box py="1rem">
               <Text fontWeight="600">
